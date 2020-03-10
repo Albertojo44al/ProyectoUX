38125ceaef11c9a2091809c801c3d9d393c0ab56
Albertojo44al <Albertojo44al@unitec.edu>
2020-03-10 15:11:19 -0600

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NavBarComponent } from './navbar/navbar.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core'
+
+
+@Component({
+    selector: 'nav-bar',
+    templateUrl: './navbar.component.html',
+    styles:[
+        `.navbar-default {
+            background-color: #5A0505;
+            color: aliceblue;
+        }`,
+        `a {
+            color: #f5f5f5;
+            text-decoration: none;
+            background-color: transparent;
+        }
+        
+        button {
+            color: #f5f5f5;
+        }
+        `
+    ]
+})
+
+export class NavBarComponent
+{
+   
+} 
